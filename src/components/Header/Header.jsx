8375240fba941daef656511630cbc3998b96f268
Prathamesh dhasade <prathameshdhasade2015@gmail.com>
2022-12-17 12:05:53 +0530

    to do website

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,38 @@
+import styles from "./header.module.css";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import { useState } from "react";
+
+ function Header({ handleAddTask }) {
+  const [title, setTitle] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    handleAddTask(title);
+    setTitle("");
+  }
+
+  function onChangeTitle(event) {
+    setTitle(event.target.value);
+  }
+
+  return (
+    <header className={styles.header}>
+      <h1>To Do List</h1>
+
+      <form onSubmit={handleSubmit} className={styles.newTaskForm}>
+        <input
+          placeholder="Add a new task"
+          type="text"
+          onChange={onChangeTitle}
+          value={title}
+        />
+        <button>
+          Create <AiOutlinePlusCircle size={20} />
+        </button>
+      </form>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/components/Tasks/Tasks.jsx
@@ -0,0 +1,38 @@
+import Task from "../Task/Task";
+import styles from "./tasks.module.css";
+
+ function Tasks({ tasks, onDelete, onComplete }) {
+  const tasksQuantity = tasks.length;
+  const completedTasks = tasks.filter((task) => task.isCompleted).length;
+
+  return (
+    <section className={styles.tasks}>
+      <header className={styles.header}>
+        <div>
+          <p>Created tasks</p>
+          <span>{tasksQuantity}</span>
+        </div>
+
+        <div>
+          <p className={styles.textPurple}>Completed tasks</p>
+          <span>
+            {completedTasks} of {tasksQuantity}
+          </span>
+        </div>
+      </header>
+
+      <div className={styles.list}>
+        {tasks.map((task) => (
+          <Task
+            key={task.id}
+            task={task}
+            onDelete={onDelete}
+            onComplete={onComplete}
+          />
+        ))}
+      </div>
+    </section>
+  );
+}
+
+export default Tasks;
